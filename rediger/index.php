054716ed53c52828838409f153dd2ac02e427ee4
DaHuntr <szymon.trojankowski@gmail.com>
2024-05-28 15:21:47 +0200

    Basic, CSS overalt osv

--- rediger/index.php
+++ rediger/index.php
@@ -9,10 +9,42 @@
     //Sett inn relevant persondata til variabelen
     $person = mysqli_fetch_array($resultat);
 ?>
-
-<form method="POST" action="rediger.php">
-    Rediger navn
-    <input type="hidden" name="personID" value="<?php echo($person['personid']) ?>">
-    <input type="text" name="navn" value="<?php echo($person['navn']) ?>">
-    <input type="submit">
-</form>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TITTEL</title>
+        <link rel="stylesheet" href="../global.css">
+    </head>
+    <body>
+        <div class="font background">
+            <div class="header headerfont">
+                HEADER
+            </div>
+            <div class="stuffgohere">
+                <div class="content-area stuffgohere">
+                    <div class="content-box">
+                        <div class="box1">
+                            <form method="POST" action="rediger.php">
+                                Rediger navn
+                                <input type="hidden" name="personID" value="<?php echo($person['personid']) ?>">
+                                <input type="text" name="navn" value="<?php echo($person['navn']) ?>">
+                                <input type="submit">
+                            </form>
+                        </div>
+                        <div class="box2">
+                        </div>
+                        <div class="box1">
+                        </div>
+                        <div class="box2">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+            © 2023 DaHunt
+            <div class="footertext">Aliqua eu consectetur elit fugiat ex pariatur elit consequat sunt proident aliquip ad qui do.</div> 
+            </div>
+        </div>
+    </body>
+</html>
--- settinn/index.php
+++ settinn/index.php
@@ -3,18 +3,42 @@
     require("../func.php");
     $dbConnection = connect();
 ?>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>DB</title>
-        <link rel="stylesheet" href="global.css">
+        <title>TITTEL</title>
+        <link rel="stylesheet" href="../global.css">
     </head>
     <body>
-        <h1>Personoversikt</h1>
-        <form method="post" action="settinn.php">
-            Skriv inn navn
-                <input type="text" name="navn">
-                <input type="submit">
-        </form>
+        <div class="font background">
+            <div class="header headerfont">
+                HEADER
+            </div>
+            <div class="stuffgohere">
+                <div class="content-area stuffgohere">
+                    <div class="content-box">
+                        <div class="box1">
+                            <h1>Personoversikt</h1>
+                            <form method="post" action="settinn.php">
+                                Skriv inn navn
+                                    <input type="text" name="navn">
+                                    <input type="submit">
+                            </form>
+                        </div>
+                        <div class="box2">
+                        </div>
+                        <div class="box1">
+                        </div>
+                        <div class="box2">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+            © 2023 DaHunt
+            <div class="footertext">Aliqua eu consectetur elit fugiat ex pariatur elit consequat sunt proident aliquip ad qui do.</div> 
+            </div>
+        </div>
     </body>
 </html>
